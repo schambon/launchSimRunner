a7c4250e922da855062018e4d2da6867846182ec
Sylvain Chambon <sylvain.chambon@mongodb.com>
2022-10-28 09:57:18 +0200

    Specify in comment for access list entry that it's simrunner

--- atlas_add_ips.sh
+++ atlas_add_ips.sh
@@ -3,6 +3,6 @@ source config.sh
 export AWS_PAGER=""
 
 expiry=$(date -v+6H -u +"%Y-%m-%dT%H:%M:%SZ")
-payload=$(aws ec2 describe-instances --filters "Name=tag:owner,Values=$OWNERTAG" "Name=tag:Name,Values=$NAMETAG" "Name=instance-state-name,Values=running" | jq -r "[ .Reservations[].Instances[] | {\"ipAddress\":.PublicIpAddress, \"deleteAfterDate\":\"$expiry\"} ]")
+payload=$(aws ec2 describe-instances --filters "Name=tag:owner,Values=$OWNERTAG" "Name=tag:Name,Values=$NAMETAG" "Name=instance-state-name,Values=running" | jq -r "[ .Reservations[].Instances[] | {\"ipAddress\":.PublicIpAddress, \"deleteAfterDate\":\"$expiry\", \"comment\":\"SimRunner auto-added\"} ]")
 
 curl --digest --user "$ATLASAPIKEY:$ATLASAPIPRIVKEY" -X POST --header "Content-Type: application/json" --header "Accept: application/json" -d "$payload" https://cloud.mongodb.com/api/atlas/v1.0/groups/$ATLASGROUP/accessList
